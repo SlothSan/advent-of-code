15d978f03823d9a3841f85ee7a5e616945bb103a
Mike Oram <Mikeoram01@gmail.com>
2022-12-03 12:09:47 +0000

    slight code tidy up

--- 2022/Day3/App.js
+++ 2022/Day3/App.js
@@ -5,13 +5,11 @@ function syncReadFile(filename) {
     return (arr = contents.split(/\r?\n/));
 }
 
-
 const alpha1 = Array.from(Array(26)).map((e, i) => i + 65);
 const alpha2 = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet1 = alpha1.map((x) => String.fromCharCode(x));
 const alphabet2 = alpha2.map((x) => String.fromCharCode(x).toLowerCase());
 const completeAlpha = [...alphabet2, ...alphabet1];
-
 let inputArray = syncReadFile("./input.txt")
 let totalSum = 0;
 
@@ -50,23 +48,6 @@ function sumGroupsOf3 (inputArray) {
     return totalSum
 }
 
-// console.log(sumAllCommonItems(inputArray));
-
+console.log(sumAllCommonItems(inputArray));
 console.log(sumGroupsOf3(inputArray))
 
-
-
-//         // for(let j = 0; j < subStr2.length; j++) {
-//         //     if (currentMatch === subStr2[j]) {
-//         //         letterMatch = subStr2[j];
-//         //         console.log(letterMatch)
-//         //         totalSum += completeAlpha.indexOf(letterMatch) + 1
-//         //         break;
-//         //     }
-//         }
-//     }
-// }
-//
-// console.log(totalSum)
-
-//Create a indexed array of a-z A-Z and return the score as that index + 1 and add it to the totalSum.
