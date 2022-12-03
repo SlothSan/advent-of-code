0df29919d098a365261e7ac218ae1eed105273d6
Mike Oram <Mikeoram01@gmail.com>
2022-12-03 11:49:16 +0000

    Part 2 solution

--- 2022/Day3/App.js
+++ 2022/Day3/App.js
@@ -15,22 +15,46 @@ const completeAlpha = [...alphabet2, ...alphabet1];
 let inputArray = syncReadFile("./input.txt")
 let totalSum = 0;
 
-for (let i = 0; i < inputArray.length; i++) {
-    let letterMatch = '';
-    let subStr1 = inputArray[i].slice(0, inputArray[i].length / 2)
-    let subStr2 = inputArray[i].slice(inputArray[i].length / 2, inputArray[i].length);
-    for (let j = 0; j < subStr1.length; j++) {
-        let currentMatch = subStr1[j];
-        if (subStr2.includes(currentMatch)) {
-            letterMatch = currentMatch;
-            totalSum += completeAlpha.indexOf(letterMatch) + 1;
-            break;
+function sumAllCommonItems (inputArray) {
+    let totalSum = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        let letterMatch = '';
+        let subStr1 = inputArray[i].slice(0, inputArray[i].length / 2)
+        let subStr2 = inputArray[i].slice(inputArray[i].length / 2, inputArray[i].length);
+        for (let j = 0; j < subStr1.length; j++) {
+            let currentMatch = subStr1[j];
+            if (subStr2.includes(currentMatch)) {
+                letterMatch = currentMatch;
+                totalSum += completeAlpha.indexOf(letterMatch) + 1;
+                break;
+            }
         }
     }
-
-    console.log(totalSum)
-
+    return totalSum
 }
+
+function sumGroupsOf3 (inputArray) {
+    let totalSum = 0;
+    for (let i = 0; i < inputArray.length; i += 3) {
+        let str1 = inputArray[i];
+        let str2 = inputArray[i+1];
+        let str3 = inputArray[i+2];
+        for (let j = 0; j < str1.length; j++) {
+            let currentLetter = str1[j]
+            if(str2.includes(currentLetter) && str3.includes(currentLetter)) {
+                totalSum += completeAlpha.indexOf(currentLetter) + 1;
+                break;
+            }
+        }
+    }
+    return totalSum
+}
+
+// console.log(sumAllCommonItems(inputArray));
+
+console.log(sumGroupsOf3(inputArray))
+
+
 
 //         // for(let j = 0; j < subStr2.length; j++) {
 //         //     if (currentMatch === subStr2[j]) {
